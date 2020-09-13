58cb0daaae4cabf59b2718f85f9f4de1114c5dba
João <jgckruger@gmail.com>
2020-09-13 18:35:42 -0300

    Added print to dynamic programming solution

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from classes.recursive import Recursive
 from classes.dynamic import Dynamic
 
-rec = Recursive(8, [1, 2, 3, 4, 5, 6, 7, 8], [2, 4, 7, 9, 11, 16, 16, 21])
-rec = Dynamic(8, [1, 2, 3, 4, 5, 6, 7, 8], [2, 4, 7, 9, 11, 16, 16, 21])
+rec = Recursive(8, [1, 2, 3, 4, 5, 6, 7, 8], [2, 4, 7, 9, 11, 16, 19, 21])
+dyn = Dynamic(8, [1, 2, 3, 4, 5, 6, 7, 8], [2, 4, 7, 9, 11, 16, 19, 21])
 print(rec.solve())
+print(dyn.solve())
