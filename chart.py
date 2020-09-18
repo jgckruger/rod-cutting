269e6cab16e75d534d2fe26717e74a242b780e41
João <jgckruger@gmail.com>
2020-09-18 16:18:29 -0300

    Fixed start of x axis

--- chart.py
+++ chart.py
@@ -4,8 +4,9 @@ from classes.dynamic import Dynamic
 rec_calls = []
 dyn_calls = []
 N = 20
+x = list(range(2, N))
 
-for i in range(2, N):
+for i in x:
     sizes = list(range(1,i))
     prices = list(range(1,i))
 
@@ -18,11 +19,11 @@ for i in range(2, N):
     dyn_calls.append(sum(dyn.calls))
 
 import matplotlib.pyplot as plt
-plt.plot(rec_calls)
-plt.plot(dyn_calls)
+plt.plot(x, rec_calls)
+plt.plot(x, dyn_calls)
 plt.ylabel('log(Number of calls)')
 plt.xlabel('N')
 plt.yscale('log')
-plt.legend(('Recursive - $\\theta(2^n)$', 'Dynamic Programming - $\\theta(n^2)$'),
+plt.legend(('Recursive - $\\theta(2^n)$', 'Dynamic Programming (Memoization) - $\\theta(n^2)$'),
            loc='upper left')
 plt.show()
