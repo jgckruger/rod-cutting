0039a727d8110a1c666e0598f494cab7a11a33ba
João <jgckruger@gmail.com>
2020-09-18 15:46:10 -0300

    Fixed example to match slides

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from classes.recursive import Recursive
 from classes.dynamic import Dynamic
 
-rec = Recursive(4, [1, 2, 3, 4, 5, 6, 7, 8], [1, 5, 8, 9, 10, 17, 17, 20])
-dyn = Dynamic(4, [1, 2, 3, 4, 5, 6, 7, 8], [2, 4, 7, 9, 11, 16, 19, 21])
+rec = Recursive(4, [1, 2, 3, 4], [1, 5, 8, 9])
+dyn = Dynamic(4, [1, 2, 3, 4], [1, 5, 8, 9])
 
 print("Recursive Solution")
 print(rec.solve())
